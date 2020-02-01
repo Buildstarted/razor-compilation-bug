be5e2b22523c639c33ca6a25d14891bfa49df4b3
Ben Dornis <ben@buildstarted.com>
2020-02-01 07:47:52 -0800

    basic repo

---
+++ Home.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication8
+{
+    public class Home : Controller
+    {
+        [HttpGet("/")]
+        public ActionResult Index()
+        {
+            return View("/Index.cshtml");
+        }
+    }
+}
